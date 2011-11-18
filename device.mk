0abb39688c0c5c6d2c2fb9bfc653b2ecaf47b7b2
Koushik Dutta <koushd@gmail.com>
2011-11-17 22:45:52 -0800

    Fix up build environment pollution.

--- device.mk
+++ device.mk
@@ -211,6 +211,4 @@ $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
 $(call inherit-product-if-exists, vendor/samsung/tuna/device-vendor.mk)
 
-BOARD_WLAN_DEVICE_REV := bcm4330_b2
-WIFI_BAND             := 802_11_ABG
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
